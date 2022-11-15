fc3d7eb3c254c85124a22ee892abb2bf51d52a3c
Pierre Nygard <pirren>
2022-11-15 20:44:29 +0100

    2018: 13 part 2

--- 2018/days/Day13.cs
+++ 2018/days/Day13.cs
@@ -1,6 +1,5 @@
 ﻿using advent_of_code_lib.attributes;
 using advent_of_code_lib.bases;
-using Serilog;
 using static advent_of_code_2018.days.Day13;
 
 namespace advent_of_code_2018.days
@@ -11,7 +10,7 @@ namespace advent_of_code_2018.days
         public override object PartOne(string[] data)
         {
             // Part 1: To help prevent crashes, you'd like to know the location of the first crash. (eg. 7,3)
-            var (positions, carts) = Parse(data);
+            var (positions, carts) = ParseData(data);
             var map = new Map
             {
                 Positions = positions,
@@ -25,9 +24,7 @@ namespace advent_of_code_2018.days
         public override object PartTwo(string[] data)
         {
             // Part 2: What is the location of the last cart at the end of the first tick where it is the only cart left?
-            // 61,74 not the right answer
-            // 126,45 not the right answer
-            var (positions, carts) = Parse(data);
+            var (positions, carts) = ParseData(data);
             var map = new Map
             {
                 Positions = positions,
@@ -38,66 +35,66 @@ namespace advent_of_code_2018.days
             return map.SimulateCollision(lookforlast: true);
         }
 
+        (char[,] positions, List<Cart> carts) ParseData(string[] data)
+        {
+            var p = new char[data.Length, data[0].Length];
+            var c = new List<Cart>();
+            for (var y = 0; y < data.Length; y++)
+            {
+                for (var x = 0; x < data[0].Length; x++)
+                {
+                    var d = data[y][x];
+                    if (char.IsWhiteSpace(d)) continue;
+                    p[y, x] = d;
+
+                    var cart = d.GetCart(x, y);
+                    if (cart != null) c.Add(cart);
+                }
+            }
+
+            return (p, c);
+        }
+
         public class Map
         {
-            public List<Position> Positions { get; set; }
+            public char[,] Positions { get; set; }
             public List<Cart> Carts { get; set; }
             public int MapSizeX { get; set; }
             public int MapSizeY { get; set; }
 
-            private int tick = 0;
-
-            //void PrintState()
-            //{
-            //    Console.WriteLine($"Iteration: {iteration}");
-            //    for (int y = 100; y < MapSizeY; y++)
-            //    {
-            //        for (int x = 0; x < MapSizeX; x++)
-            //        {
-            //            if (!Positions.Any(p => p.P.x == x && p.P.y == y)) {
-            //                Console.Write(" ");
-            //                continue;
-            //            }
-            //            var cart = Carts.FirstOrDefault(c => c.At.P.x == x && c.At.P.y == y);
-            //            if (cart != null) Console.Write(cart.Figure);
-            //            else Console.Write(Positions.FirstOrDefault(p => p.P.x == x && p.P.y == y)?.Type);
-            //        }
-            //        Console.Write("\r\n");
-            //    }
-            //    Console.Write("\r\n");
-            //}
+            public void SortCarts()
+            {
+                Carts = Carts.OrderBy(c => c.Y).ThenBy(c => c.X).ToList();
+            }
 
             public (int x, int y) SimulateCollision(bool lookforlast = false)
             {
-                
+                SortCarts();   
                 while (Carts.Count(x => x.Alive) > 1)
                 {
-                    //PrintState();
-                    //Console.ReadLine();
-                    
-                    foreach(var cart in Carts.Where(x => x.Alive).OrderBy(x => x.At.P.x).ThenBy(x => x.At.P.y))
+                    foreach (var cart in Carts)
                     {
                         if (!cart.Alive) continue;
-                        var nextcartpos = cart.GetNextPosition(Positions);
+                        var pos = cart.NextPosition(); 
 
-                        if (nextcartpos.IsIntersection()) cart.TurnAtIntersection();
-                        else if (nextcartpos.IsCurve()) cart.Turn(nextcartpos.Type);
+                        if (Positions[pos.y, pos.x] == '+') cart.TurnAtIntersection();
+                        else if (Positions[pos.y, pos.x].IsCurve()) cart.Turn(Positions[pos.y, pos.x]);
 
-                        //Log.Debug($"Cart {cart.Id} {cart.Figure} prev pos: {cart.At.P} - {cart.At.Type}, next pos: {nextcartpos.P} - {nextcartpos.Type}");
 
-                        cart.At = nextcartpos;
-                        if(Carts.Any(c => c.At == cart.At && c.Id != cart.Id))
-                        {
-                            if (!lookforlast) return cart.At.P;
+                        cart.X = pos.x;
+                        cart.Y = pos.y;
 
-                            foreach(var c2 in Carts.Where(x => x.At == cart.At))
-                                c2.Alive = false;
-                        }
+                        if (!Carts.Any(c => c.Alive && c.X == pos.x && c.Y == pos.y && c != cart)) continue;
+
+                        cart.Alive = false;
+                        foreach (var c2 in Carts.Where(c => (c.X, c.Y) == pos && c.Alive)) c2.Alive = false;
+
+                        if (!lookforlast && Carts.Any(x => !x.Alive)) return new(pos.x, pos.y);
+
                     }
-
-                    tick++;
+                    SortCarts();
                 }
-                return Carts.Single(x => x.Alive).At.P;
+                return Carts.Where(x => x.Alive).Select(cart => (cart.X, cart.Y)).Single();
             }
         }
 
@@ -110,10 +107,11 @@ namespace advent_of_code_2018.days
                 { TurnFor.Right, TurnFor.Left }
             };
 
-            public int Id { get; set; } // debug
-            public char Figure { get; set; }
             public bool Alive { get; set; } = true;
-            public Position At { get; set; } = null;
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Dx { get; set; }
+            public int Dy { get; set; }
 
             private TurnFor nextTurn = TurnFor.Left;
             public TurnFor NextTurn()
@@ -123,76 +121,36 @@ namespace advent_of_code_2018.days
                 return turn;
             }
 
-            public Position GetNextPosition(List<Position> positions)
-                => Figure switch
-                {
-                    '>' => positions.First(x => x.P.y == At.P.y && x.P.x == At.P.x + 1),
-                    'v' => positions.First(x => x.P.y == At.P.y + 1 && x.P.x == At.P.x),
-                    '<' => positions.First(x => x.P.y == At.P.y && x.P.x == At.P.x - 1),
-                    '^' => positions.First(x => x.P.y == At.P.y - 1 && x.P.x == At.P.x),
-                };
+            public (int x, int y) NextPosition() => (X + Dx, Y + Dy);
 
             public void TurnAtIntersection()
             {
+                void T(int i)
+                {
+                    if (Dx == -1) { Dy = 1 * i; Dx = 0; }
+                    else if (Dx == 1) { Dy = -1 * i; Dx = 0; }
+                    else if (Dy == -1) { Dy = 0; Dx = -1 * i; }
+                    else if (Dy == 1) { Dy = 0; Dx = 1 * i; }
+                }
+
                 var direction = NextTurn();
-                if (direction == TurnFor.Left)
-                {
-                    Figure = Figure switch
-                    {
-                        '>' => '^',
-                        'v' => '>',
-                        '<' => 'v',
-                        '^' => '<',
-                        _ => throw new Exception()
-                    };
-                }
-                else if (direction == TurnFor.Right)
-                {
-                    Figure = Figure switch
-                    {
-                        '>' => 'v',
-                        'v' => '<',
-                        '<' => '^',
-                        '^' => '>',
-                        _ => throw new Exception()
-                    };
-                }
+                if (direction == TurnFor.Left) T(1);
+                else if (direction == TurnFor.Right) T(-1);
             }
 
             public void Turn(char turn)
             {
-                if (turn == '/')
+                void T(int i)
                 {
-                    Figure = Figure switch
-                    {
-                        '>' => '^',
-                        'v' => '<',
-                        '<' => 'v',
-                        '^' => '>',
-                        _ => throw new Exception()
-                    };
+                    if (Dx == -1) { Dy = 1 * i; Dx = 0; }
+                    else if (Dx == 1) { Dy = -1 * i; Dx = 0; }
+                    else if (Dy == -1) { Dy = 0; Dx = 1 * i; }
+                    else if (Dy == 1) { Dy = 0; Dx = -1 * i; }
                 }
-                else if (turn == '\\')
-                {
-                    Figure = Figure switch
-                    {
-                        '>' => 'v',
-                        'v' => '>',
-                        '<' => '^',
-                        '^' => '<',
-                        _ => throw new Exception()
-                    };
-                }
-            }
-        }
 
-        public class Position
-        {
-            public (int x, int y) P { get; set; }
-            public char Type { get; set; }
-            public static char[] CurveTracks { get; } = new char[] { '/', '\\' };
-            public bool IsIntersection() => Type == '+';
-            public bool IsCurve() => CurveTracks.Contains(Type);
+                if (turn == '/') T(1);
+                else if (turn == '\\') T(-1);
+            }
         }
 
         public enum TurnFor
@@ -201,54 +159,26 @@ namespace advent_of_code_2018.days
             Straight,
             Right
         }
-
-        (List<Position> positions, List<Cart> carts) Parse(string[] data)
-        {
-            var p = new List<Position>();
-            var c = new List<Cart>();
-            var cartcount = 0;
-            for (var y = 0; y < data.Length; y++)
-            {
-                for (var x = 0; x < data[0].Length; x++)
-                {
-                    var d = data[y][x];
-                    if (char.IsWhiteSpace(d)) continue;
-
-                    var position = new Position
-                    {
-                        P = (x, y),
-                        Type = d.UnderlyingTrack()
-                    };
-                    p.Add(position);
-
-                    var cart = d.GetCart(position);
-                    if (cart != null)
-                    {
-                        cartcount++;
-                        cart.Id = cartcount;
-                        c.Add(cart);
-                    }
-                }
-            }
-
-            return (p, c);
-        }
     }
 
-    static class Ext13
+    internal static class Ext13
     {
         public static char[] CartTypes { get; } = new char[] { '>', 'v', '<', '^' };
         public static char[] AllTracks { get; } = new char[] { '|', '-', '+', '/', '\\' };
-        public static char[] PathTracks { get; } = new char[] { '|', '-' };
+        public static char[] CurveTracks { get; } = new char[] { '/', '\\' };
 
-        public static Cart? GetCart(this char ch, Position pos) => CartTypes.Contains(ch) ? new Cart { Figure = ch, At = pos } : null;
-
-        public static char UnderlyingTrack(this char ch)
+        public static bool IsCurve(this char ch) => CurveTracks.Contains(ch);
+        public static Cart? GetCart(this char ch, int x, int y)
         {
-            if (AllTracks.Contains(ch)) return ch;
-            if (new char[] { '^', 'v' }.Contains(ch)) return '|';
-            if (new char[] { '<', '>' }.Contains(ch)) return '-';
-            throw new Exception("Unhandled track part");
+            if (!CartTypes.Contains(ch)) return null;
+            return ch switch
+            {
+                '>' => new Cart { Dx = 1, Dy = 0, X = x, Y = y },
+                'v' => new Cart { Dx = 0, Dy = 1, X = x, Y = y },
+                '<' => new Cart { Dx = -1, Dy = 0, X = x, Y = y },
+                '^' => new Cart { Dx = 0, Dy = -1, X = x, Y = y },
+                _ => throw new NotImplementedException()
+            };
         }
     }
 }
