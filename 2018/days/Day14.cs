6766c7368491bd5d730f43921841ea1dc5c5f0b5
Pierre Nygard <pirren>
2022-11-26 23:40:51 +0100

    2018: 14

--- 2018/days/Day14.cs
+++ 2018/days/Day14.cs
@@ -1,8 +1,6 @@
 ﻿using advent_of_code_lib.attributes;
 using advent_of_code_lib.bases;
 using advent_of_code_lib.extensions;
-using static advent_of_code_2018.days.Day09;
-using static advent_of_code_2018.days.Day13;
 
 namespace advent_of_code_2018.days
 {
@@ -12,124 +10,52 @@ namespace advent_of_code_2018.days
         public override object PartOne(string[] data)
         {
             // Part 1: What are the scores of the ten recipes immediately after the number of recipes in your puzzle input?
-            // 2151055104 not right
-            var recipes = new LinkedRecipesList
+            int target = data[0].ToInt();
+            List<int> nodes = new() { 3, 7 };
+
+            int first = 0;
+            int second = 1;
+
+            while (nodes.Count < target + 10)
             {
-                TargetCountRecipes = 2018//data[0].ToInt()
-            };
-            return recipes.MakeChocolate();
+                var nextsum = nodes[first] + nodes[second];
+                if (nextsum > 9) nodes.Add(1);
+                nodes.Add(nextsum % 10);
+
+                first = (first + nodes[first] + 1) % nodes.Count;
+                second = (second + nodes[second] + 1) % nodes.Count;
+            }
+
+            return string.Join(string.Empty, nodes.GetRange(target, 10).Select(x => x.ToString()).ToArray());
         }
 
         public override object PartTwo(string[] data)
         {
-            // Part 2: -
-            return 0;
-        }
+            // Part 2: How many recipes appear on the scoreboard to the left of the score sequence in your puzzle input?
+            int target = data[0].ToInt();
+            List<int> targetSequence = new();
+            for(int i = target.CountDigits(); i > 0; i--) targetSequence.Add(target.NumberAt(i - 1));
+            
+            List<int> nodes = new() { 3, 7 };
 
-        internal class LinkedRecipesList
-        {
-            public List<LinkedNode> Nodes { get; set; } = new();
+            int first = 0;
+            int second = 1;
 
-            public Cook FirstElf { get; set; } = null;
-            public Cook SecondElf { get; set; } =  null;
-            public int CookingElvesCount { get; set; } = 2;
-            public int TargetCountRecipes { get; set; }
-
-            public string MakeChocolate()
+            while (!nodes.TakeLast(targetSequence.Count).SequenceEqual(targetSequence))
             {
-                var firstnode = new LinkedNode { Value = 3, NodeId = 1 };
-                var secondnode = new LinkedNode { Value = 7, NodeId = 2 };
-
-                // connect nodes
-                firstnode.Connect(secondnode);
-                firstnode.ConnectEdge(secondnode);
-
-                // let elves track their current nodes
-                FirstElf = new Cook { Recipe = firstnode };
-                SecondElf = new Cook { Recipe = secondnode };
-
-                Nodes.Add(firstnode);
-                Nodes.Add(secondnode);
-
-                while (true)
+                var nextsum = nodes[first] + nodes[second];
+                if (nextsum > 9)
                 {
-                    for(var i = 0; i < CookingElvesCount; i++)
-                    {
-                        CreateNewRecipes();
-
-                        if (Nodes.Count < TargetCountRecipes) continue;
-
-                        var relativeNode = Nodes.Count > TargetCountRecipes ? Nodes.Last().Previous : Nodes.Last();
-
-                        // get the next 10
-                        for (int n = Nodes.Count - TargetCountRecipes; n < 10; n++)
-                            CreateNewRecipes();
-
-                        return string.Join(string.Empty, GetNextTenNodes(relativeNode).Select(x => x.Value.ToString()).ToArray());
-                    }
+                    nodes.Add(1);
+                    if (nodes.TakeLast(targetSequence.Count).SequenceEqual(targetSequence)) break;
                 }
+                nodes.Add(nextsum % 10);
+
+                first = (first + nodes[first] + 1) % nodes.Count;
+                second = (second + nodes[second] + 1) % nodes.Count;
             }
 
-            void CreateNewRecipes()
-            {
-                var score = FirstElf.Recipe.Value + SecondElf.Recipe.Value;
-                for (var i = score.CountDigits(); i > 0; i--)
-                {
-                    var newnode = new LinkedNode { Value = score.NumberAt(i - 1), Next = Nodes.Last(), NodeId = Nodes.Count + 1 };
-                    Nodes.Last().Connect(newnode); 
-                    Nodes.First().ConnectEdge(newnode); 
-                    Nodes.Add(newnode);
-                }
-
-                FirstElf.Recipe = Move(FirstElf.Recipe, FirstElf.Recipe.Value + 1);
-                SecondElf.Recipe = Move(SecondElf.Recipe, SecondElf.Recipe.Value + 1);
-            }
-
-
-            public class Cook
-            {
-                public LinkedNode Recipe { get; set; }
-            }
-
-            IEnumerable<LinkedNode> GetNextTenNodes(LinkedNode relativeNode)
-            {
-                List<LinkedNode> nextnnodes = new();
-                for(int i = 0; i < 10; i++)
-                {
-                    relativeNode = relativeNode.Next;
-                    nextnnodes.Add(relativeNode);
-                }
-                return nextnnodes;
-            }
-
-            LinkedNode Move(LinkedNode node, int steps)
-            {
-                if (steps == 0) return node;
-                return Move(node.Next, steps - 1);
-            }
-        }
-
-        internal class LinkedNode
-        {
-            public int NodeId { get; set; }
-            public int Value { get; set; }
-            public LinkedNode Previous { get; set; } = null;
-            public LinkedNode Next { get; set; } = null;
-        }
-    }
-
-    internal static class Ext14
-    {
-        public static void Connect(this Day14.LinkedNode prev, Day14.LinkedNode next)
-        {
-            prev.Next = next;
-            next.Previous = prev;
-        }
-
-        public static void ConnectEdge(this Day14.LinkedNode first, Day14.LinkedNode last)
-        {
-            first.Previous = last;
-            last.Next = first;
+            return nodes.SkipLast(targetSequence.Count).Count();
         }
     }
 }
