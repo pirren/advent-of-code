ee1bf74330a2ac166c1bb8c3cf246bba4e0726d4
pirren <pierre.nygard@gmail.com>
2022-12-07 18:46:04 +0100

    2022: 6

---
+++ 2022/days/Day06.cs
@@ -0,0 +1,24 @@
+﻿using advent_of_code_lib.attributes;
+using advent_of_code_lib.bases;
+
+namespace advent_of_code_2022.days
+{
+    [ProblemInfo(6, "Tuning Trouble")]
+    public class Day06 : SolverBase
+    {
+        public override object PartOne(string[] data)
+        {
+            // Part 1: How many characters need to be processed before the first start-of-packet marker is detected?
+            return GetMarker(data[0], 4);
+        }
+
+        public override object PartTwo(string[] data)
+        {
+            // Part 2: How many characters need to be processed before the first start-of-message marker is detected
+            return GetMarker(data[0], 14);
+        }
+
+        private int GetMarker(string buffer, int type) 
+            => Enumerable.Range(0, buffer.Length - type - 1).First(i => buffer[i..(i + type)].Distinct().Count() == type) + type;
+    }
+}
